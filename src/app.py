6dfe0caf75a89c425431b83ee7910c5dad5f674a
qbazi <qbazilla@hotmail.com>
2020-06-12 12:40:31 +0200

    fix after merge

--- src/app.py
+++ src/app.py
@@ -17,7 +17,7 @@ external_scripts = []
 
 tabs_dict = {
     "tab1": tabs.StatesTabContent(),
-    "tab2": tabs.ExampleTabContent(),
+    "tab2": tabs.OlympicTabContent(),
     "tab3": tabs.ExampleTabContent(),
     "tab4": tabs.ExampleTabContent(),
     "tab5": tabs.ExampleTabContent()
